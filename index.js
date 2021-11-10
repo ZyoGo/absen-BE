e83398535d01fbf5fd1752c80df53a46c6f19cb2
Harly <academyynetworking@gmail.com>
2021-11-10 20:46:29 +0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+
+const port = 3000;
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+app.listen(port, () => {
+  console.log(`🚀 Server running on http://localhost:${port}`);
+});
